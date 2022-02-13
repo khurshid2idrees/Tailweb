c99ff00bd2c07edb0ee67bd90c797be52d93d75e
khurshid2idrees <73442661+khurshid2idrees@users.noreply.github.com>
2022-02-13 06:48:39 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+export default function Header() {
+    return (
+        
+        <div className="container bg-purple-800 w-full h-16 flex justify-end">
+            <div className="box">
+                <ul className="mr-16 py-4">
+                    <a className='px-4 hover:text-white' href="#">Home</a>
+                    <a className='px-4 hover:text-white' href="#">About</a>
+                    <a className='px-4 hover:text-white' href="#">Contact Us</a>
+                    <a className='px-4 hover:text-white' href="#">Industries</a>   
+                </ul>
+            </div>
+        </div>
+    )
+
+
+}
+
+
+
+
+
+
---
+++ src/Components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Main() {
+  return 
+  (
+  <div className="container h-32 w-full bg-purple-400">
+   <h1>This is The  content part of the webpage</h1>
+  </div>
+  );
+}
